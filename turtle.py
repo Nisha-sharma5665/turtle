5ea0e350e6a4a636d3320f6a748cfe5a05d5e6fb
Nisha-sharma5665 <133190600+Nisha-sharma5665@users.noreply.github.com>
2023-08-01 12:01:46 +0530

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,21 @@
+from turtle import *
+import turtle
+t=turtle.Turtle()
+t.right(360)
+t.forward(100)
+t.left(120)
+t.backward(100)
+t.right(360)
+t.forward(100)
+t.left(120)
+t.backward(100)
+t.right(360)
+t.forward(100)
+t.left(120)
+t.backward(100)
+t.right(360)
+t.forward(100)
+t.left(120)
+t.backward(100)
+turtle.done()
+
